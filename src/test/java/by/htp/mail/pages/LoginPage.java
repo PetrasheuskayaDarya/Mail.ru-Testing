e9fb174e7f0e3c8e71978610257c6a87951e802c
Darya <darya.petrasheuskaya@gmail.com>
2018-07-25 21:24:40 +0300

    .gitignore and LoginPage are updated

--- src/test/java/by/htp/mail/pages/LoginPage.java
+++ src/test/java/by/htp/mail/pages/LoginPage.java
@@ -1,5 +1,45 @@
 package by.htp.mail.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends AbstractPage {
+	
+	private final String BASE_URL = "https://mail.ru";
+
+	@FindBy(xpath = "//input[@id='mailbox:login']")
+	private WebElement inputLogin;
+
+	@FindBy(xpath = "//input[@id='mailbox:password']")
+	private WebElement inputPassword;
+
+	@FindBy(xpath = "//input[@type='submit']")
+	private WebElement buttonSubmit;
+
+	public LoginPage(WebDriver driver)
+	{
+		super(driver);
+		PageFactory.initElements(this.driver, this);
+	}
+
+	@Override
+	public void openPage()
+	{
+		driver.navigate().to(BASE_URL);
+	}
+
+	public void login(String username, String password)
+	{
+		inputLogin.sendKeys(username);
+		inputPassword.sendKeys(password);
+		buttonSubmit.click();
+	}
+
+	public void checkTitleMainPage() {
+		String mainPageTitle = mainPage.getTitle();
+		
+	}
 
 }
